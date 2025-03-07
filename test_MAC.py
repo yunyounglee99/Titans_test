41da19edcf4df02476dc69864a0e87d75dddd868
nyoung <kembel0116@gmail.com>
2025-03-07 21:07:50 +0900

    .

---
+++ test_MAC.py
@@ -0,0 +1,19 @@
+import torch
+from titans_pytorch import MemoryAsContextTransformer
+
+transformer = MemoryAsContextTransformer(
+  num_tokens = 256,
+  dim = 256,
+  depth = 2,
+  segment_len = 128,
+  num_persist_mem_tokens = 4,
+  num_longterm_mem_tokens = 16
+)
+
+token_ids = torch.randint(0, 256, (1, 1023))
+
+loss = transformer(token_ids, return_loss = True)
+loss.backward()
+
+sampled = transformer.sample(token_ids[:, :4], 512)
+sampled[0]
