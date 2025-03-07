d70fe2885362de901d97111ddb3f904a0c55c480
nyoung <kembel0116@gmail.com>
2025-03-07 19:52:19 +0900

    .

--- test.py
+++ test.py
@@ -1,4 +1,14 @@
 import torch
-import titans_pytorch
+from titans_pytorch import NeuralMemory
 
-print(dir (titans_pytorch))
+device = torch.device("mps")
+
+mem = NeuralMemory(
+  dim = 384,
+  chunk_size = 64
+).to(device)
+
+seq = torch.randn(2, 1024, 384).to(device)
+retrieved, mem_state = mem(seq)
+
+assert seq.shape == retrieved.shape
